db3f80e6ad9c97a6565262edf5e7e1d6427c22d0
lhpaul11@gmail.com <>
2012-08-25 12:18:51 -0500

    Primera prueba

--- iWuanaGo/js/map.js
+++ iWuanaGo/js/map.js
@@ -1,180 +1,161 @@
 (function($) {
 
-  var Core = window.Core || Core || {};
+	var Core = window.Core || Core || {};
 
-  Core.map = {
+	Core.map = {
 
-    init: function() {
-      //Core.auth.requireSession();
-      Core.map.bindEvents();
-      $("#map_canvas").height(Core.body_height - ($("#flipbox").height() + $("#header").height()));
-      $('#map_canvas').gmap();
-    },
+		init : function() {
+			//Core.auth.requireSession();
+			Core.map.current_location_image = new google.maps.MarkerImage('images/current_location_icon.png');
+			Core.map.bindEvents();
+			Core.map.putMap();
+		},
 
-    bindEvents: function() {
-      $("#map-pg").bind("pageshow", function() {
-        window.plugins.mapKit.showMap();
-      });
-      $("#map-pg").bind("pagebeforehide", function() {
-        window.plugins.mapKit.hideMap();
-      });
+		bindEvents : function() {
 
-      $('#locate').live('click', function() {
-        //Core.auth.logout();
-        Core.map.changeMessage("loadind events...", "#39AB3E");
-        return false;
-      });
-      $('#settings').live('click', function() {
-        Core.map.messageBox.changeMessage("Downloading events...", "#0D12A4");
-        Core.request_events({
-          latitude: -33.498962,
-          longitude: -70.616562
-        }, 'todos', 'week', 10, {
-          onSuccess: Core.map.add_events.onSuccess,
-          onError: Core.map.add_events.onError,
-          onDenied: Core.map.add_events.onDenied,
-          onComplete: Core.map.add_events.onComplete
-        });
-      console.log("funciona");
+			$('#map-pg').live('pageshow', function() {
+			});
 
+			$('#locate').live('click', function() {
+				Core.map.current_marker.setMap(null);
+				Core.map.go_to_user_location();
+				return false;
 
-      });
-    },
+			});
+			$('#settings').live('click', function() {
+				Core.map.messageBox.changeMessage("Downloading events...", "#0D12A4");
+				Core.request_events({
+					latitude : -33.498962,
+					longitude : -70.616562
+				}, 'todos', 'week', 10, {
+					onSuccess : Core.map.add_events.onSuccess,
+					onError : Core.map.add_events.onError,
+					onDenied : Core.map.add_events.onDenied,
+					onComplete : Core.map.add_events.onComplete
+				});
 
-    getPosition: function() {
-      var dfd = $.Deferred();
-      navigator.geolocation.getCurrentPosition(
+			});
+		},
 
-      function(position) {
-        dfd.resolve(position)
-      }, function(error) {
-        dfd.resolve({
-          coords: {
-            latitude: -33.498962,
-            longitude: -70.616562
-          }
-        })
-      })
-      return dfd.promise();
-    },
+		goToPosition : function() {
 
-    putMap: function(coords) {
-      var options = {
-        buttonCallback: "cbMapCallback",
-        height: Core.body_height - $("#flipbox").height(),
-        offsetTop: $("#header").height() + $("#flipbox").height(),
-        diameter: 1000,
-        lat: coords.latitude,
-        lon: coords.longitude
-      };
+		},
 
-      // setTimeout(function() {
-      //   Core.map.locked = false;
-      // }, 1000);
-      window.plugins.mapKit.showMap();
-      window.plugins.mapKit.setMapData(options);
-    },
+		putMap : function() {
+			$("#map_canvas").height(Core.body_height - ($("#flipbox").height() + $("#header").height()));
+			var mapOptions = {
+				zoom : 14,
+				mapTypeId : google.maps.MapTypeId.ROADMAP
+			};
+			map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-    add_events: {
+			Core.map.go_to_user_location();
+		},
+		go_to_user_location : function() {
+			if (!navigator.geolocation) {
+				alert("Your device does not support geolocation");
+				return;
+			}
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+				map.setCenter(pos);
+				
+				Core.map.current_marker = new google.maps.Marker({
+					position : pos,
+					map : map,
+					icon : Core.map.current_location_image
+				});
+				Core.map.current_marker.setAnimation(google.maps.Animation.BOUNCE);
+			}, function() {
+				map.setCenter(new google.maps.LatLng(60, 105));
+				alert("Error in geolocation");
+			});
 
-      onSuccess: function(data) {
-        var newpins = [];
-        $.each(data, function(i, evnt) {
-          newpins.push({
-            lat: evnt.latitude,
-            lon: evnt.longitude,
-            title: evnt.name,
-            pinColor: "purple",
-            index: 0,
-            selected: false
-          })
-        });
-        if (newpins.length > 0) window.plugins.mapKit.addMapPins(newpins);
+		},
+		add_events : {
 
-        Core.map.messageBox.changeMessage("Ready", "#39AB3E");
-      },
+			onSuccess : function(data) {
+				var newpins = [];
+				// $.each(data, function(i, evnt) {
+				// newpins.push({
+				// lat: evnt.latitude,
+				// lon: evnt.longitude,
+				// title: evnt.name,
+				// pinColor: "purple",
+				// index: 0,
+				// selected: false
+				// })
+				// });
+				// if (newpins.length > 0) window.plugins.mapKit.addMapPins(newpins);
 
-      onError: function(data) {
-        Core.map.messageBox.changeMessage("Conection error", "#DA0122");
-      },
+				Core.map.messageBox.changeMessage("Ready", "#39AB3E");
+			},
 
-      onDenied: function(data) {},
+			onError : function(data) {
+				Core.map.messageBox.changeMessage("Conection error", "#DA0122");
+			},
 
-      onComplete: function(data) {
+			onDenied : function(data) {
+			},
 
-      }
-    },
+			onComplete : function(data) {
 
-    putPins: function(pins) {
-      app.mapPins = [];
-      $.each(pins, function(i, pin) {
-        app.mapPins.push({
-          lat: pin.latitude,
-          lon: pin.longitude,
-          title: pin.name,
-          pinColor: "purple",
-          index: 0,
-          selected: false
-        })
-      })
-      if (app.mapPins.length > 0) window.plugins.mapKit.addMapPins(app.mapPins);
-    },
+			}
+		},
 
-    getBBOX: function(location) {
-      return [location.lon - (location.deltaX / 2), location.lat - (location.deltaY / 2), location.lon + (location.deltaX / 2), location.lat + (location.deltaY / 2)].join(",");
-    },
+		putPins : function(pins) {
+			app.mapPins = [];
+			$.each(pins, function(i, pin) {
+				app.mapPins.push({
+					lat : pin.latitude,
+					lon : pin.longitude,
+					title : pin.name,
+					pinColor : "purple",
+					index : 0,
+					selected : false
+				})
+			})
+			if (app.mapPins.length > 0)
+				window.plugins.mapKit.addMapPins(app.mapPins);
+		},
 
-    onMapMove: function(lat, lon, deltaY, deltaX) {
-      // app.lastLocation = {
-      //   lat: parseFloat(lat),
-      //   lon: parseFloat(lon),
-      //   deltaY: parseFloat(deltaY),
-      //   deltaX: parseFloat(deltaX)
-      // }
+		onMapMove : function(lat, lon, deltaY, deltaX) {
 
-      // app.lastLocation.bbox = getBBOX(app.lastLocation);
+		},
+		messageBox : {
+			busy : false,
+			messages : [],
+			changeMessage : function(msg, col) {
+				Core.map.messageBox.messages.push({
+					message : msg,
+					color : col
+				});
+				if (Core.map.messageBox.busy)
+					return;
+				Core.map.messageBox.busy = true;
+				Core.map.messageBox.displayMessage(Core.map.messageBox.messages.shift());
 
-      // window.plugins.mapKit.clearMapPins();
-      // couch.get("http://open211.org/api/services?bbox=" + app.lastLocation.bbox).then(function(results) {
-      //   putPins(results.rows.map(function(row) {
-      //     return row.value;
-      //   }));
-      // })
-      console.log("movido");
+			},
+			displayMessage : function(data) {
+				$("#flipbox").flip({
+					direction : 'bt',
+					color : data.color,
+					content : data.message,
+					onEnd : Core.map.messageBox.end_of_animation
+				});
+			},
+			end_of_animation : function() {
+				if (Core.map.messageBox.messages.length > 0)
+					Core.map.messageBox.displayMessage(Core.map.messageBox.messages.shift());
+				else
+					Core.map.messageBox.busy = false;
+			}
+		}
 
-    },
-    messageBox: {
-      busy: false,
-      messages: [],
-      changeMessage: function(msg, col) {
-        Core.map.messageBox.messages.push({message: msg, color: col});
-        if(Core.map.messageBox.busy)
-          return;
-        Core.map.messageBox.busy = true;
-        Core.map.messageBox.displayMessage(Core.map.messageBox.messages.shift()); 
-        
-      },
-      displayMessage: function(data){
-        $("#flipbox").flip({
-          direction: 'bt',
-          color: data.color,
-          content: data.message,
-          onEnd: Core.map.messageBox.end_of_animation
-          }
-        );
-      },
-      end_of_animation: function(){
-        if(Core.map.messageBox.messages.length > 0)
-              Core.map.messageBox.displayMessage(Core.map.messageBox.messages.shift());
-            else
-              Core.map.messageBox.busy = false;
-      }
-    }
+	};
 
-  };
+	$(Core.map.init);
 
-  $(Core.map.init);
-
-  window.Core = Core;
+	window.Core = Core;
 
 })(jQuery);
